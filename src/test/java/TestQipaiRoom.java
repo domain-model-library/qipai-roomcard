508362d387993139d4f0eb0e88d8f5dfc6d1293b
zhengchengdong <zhengchengdong@gmail.com>
2025-06-26 19:29:02 +0800

    fixing

--- src/test/java/TestQipaiRoom.java
+++ src/test/java/TestQipaiRoom.java
@@ -1,4 +1,15 @@
 import dml.qipairoom.entity.QipaiRoomBase;
 
 public class TestQipaiRoom extends QipaiRoomBase {
+    private String no;
+
+    @Override
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    @Override
+    public String getNo() {
+        return no;
+    }
 }
